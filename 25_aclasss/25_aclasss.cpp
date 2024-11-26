8ec2e5d80af60f87bab1597976b0df6264eaeb41
Maks <rubelmaksum2404@gmail.com>
2024-11-26 21:41:24 +0200

    new

--- 25_aclasss/25_aclasss.cpp
+++ 25_aclasss/25_aclasss.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <array>
 #include <algorithm>
+#include <random>
+
 using namespace std;
 
 template<typename ContType>
@@ -18,6 +20,7 @@ void print(const ContType& cont, const string& prompt = "")
 }
 int main()
 {
+	srand(0);
 	vector<int> v1 (10);
 	vector<int> v2(10,0);
 	vector<int> v4(10);
@@ -49,6 +52,96 @@ int main()
 	vector<int> v5(b-2);
 
 	copy(v3.begin() + 1, v3.end() - 1, v5.begin());
+	print(v5, "Vector v5");
+	///////////////////////////////////
+	cout << "\n\n";
+	cout << "\n\n";
+
+	print(v3, "Vector v3");
+	cout << "Vector v1 ";
+	for (auto it = v1.rbegin(); it != v1.rend(); it++)
+	{
+		cout << *it << "\t";
+	}cout << endl;
+	cout << "\n\n";
+
+	for (size_t i = 0; i < v2.size(); i++)
+	{
+		v2[i] = i + 1;
+	}
+	
+
+	v2.erase(v2.begin() + 1);
+	v2.erase(v2.end() - 1);
+
+	for (size_t i = 0; i < v2.size(); i++)
+	{
+		int a = rand() % 10;
+		swap(v2[i],v2[a]);
+	}
+	print(v2, "Vector v2");
+	cout << "\n\n";
+
+	vector<int> result;
+	
+	for (size_t i = 0; i < v4.size(); i++)
+	{
+		result.push_back(v4[i]);
+		result.push_back(v4[i]);
+
+
+	}
+	print(result, "Vector v4");
+
+	cout << "\n\n";
+
+	v5.clear();
+	for (size_t i = 0; i < v4.size(); i++)
+	{
+		if (v4[i] % 2 == 0)
+		{
+			v5.push_back(v4[i]);
+		}
+
+
+	}
+	transform(v5.begin(), v5.end(), v5.begin(), [](auto value) {return value / 2; });
+
+	print(v5, "Vector v5");
+	cout << "\n\n";
+
+
+	int value = 5;
+	auto itF = find(v5.begin(), v5.end(), value);
+	while (itF != end(v5))
+	{
+		cout << "Found value :: " << value << " in index " << itF - begin(v5) << endl;
+		itF = find(itF + 1, v5.end(), value);
+	}
+
+	cout << "Count of value " << value << " = " << count(v5.begin(), v5.end(), value) << endl;
+
+
+
+	cout << "\n\n";
+	vector<int> v6(v4.size());
+
+
+	print(v4, "Vector v4");
+	print(v5, "Vector v5");
+
+
+	copy(v4.begin(), v4.end(), v6.begin());
+	v4.clear();
+	v4.resize(v5.size());
+	copy(v5.begin(), v5.end(), v4.begin());
+	v5.clear();
+	v5.resize(v6.size());
+
+	copy(v6.begin(), v6.end(), v5.begin());
+
+
+	print(v4, "Vector v4");
 	print(v5, "Vector v5");
 
 
